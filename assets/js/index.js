11605a2dbff909f1da80a75d44e97fc2ac80b1ce
iNeverNobody <iNever001029@outlook.com>
2022-12-11 15:17:34 +0800

    More sentense

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,27 @@
 window.onload=function(){
     console.log("Hello World!");  
-    document.getElementById('foot').innerHTML=`<hr><CENTER><a href="https://github.com/iNeverNobody" class="ft" target="_blank">&copy;iNeverNobody ,2021~2022</a><br><a href="https://icp.gov.moe/?keyword=20222133" target="_blank" class="ft">萌ICP备20222133号</a><br><a class="ft" href="">网站已经存在了<span id="timer"></span></a><br><a href="" class="ft" onclick="window.close()">离开</a></CENTER>`;
+    document.getElementById('foot').innerHTML=`<hr><CENTER><a href="https://github.com/iNeverNobody" class="ft" target="_blank">&copy;iNeverNobody ,2021~2022</a><br><a href="https://icp.gov.moe/?keyword=20222133" target="_blank" class="ft">萌ICP备20222133号</a><br><a class="ft" href="">网站已经存在了<span id="timer"></span></a><br><p class="ft" onclick="window.close()">离开</p></CENTER>`;
     startCounting();
-    document.getElementById('say').innerHTML=`<center><h1>那些夏天，就像青春一样回不来。<small>--《安和桥》</small></h1></center>`
+    let sayings=[
+        "那些夏天，就像青春一样回不来。",
+        "一星陨落，黯淡不了星空灿烂；一花凋零，荒芜不了整个春天。",
+        "一个人并不是生来要被打败的，你尽可以把他消灭掉，可就是打不败他。",
+        "心生，种种魔生；心灭，种种魔灭。",
+        "天亮之前有一个时间是非常暗的，星也没有，月亮也没有。",
+        "我宁愿靠自己的力量，打开我的前途，而不愿求有力者垂青。",
+    ]
+    let author=[
+        "《安和桥》",
+        "巴尔扎克",
+        "海明威《老人与海》",
+        "《西游记》",
+        "茅盾《子夜》",
+        "雨果《悲惨世界》",
+    ]
+    let pos=Math.floor(Math.random()*sayings.length)
+    let say=sayings[pos];
+    let auth=author[pos];
+    document.getElementById('say').innerHTML=`<center><h1>${say}<br><small>——${auth}</small></h1></center>`
 }
 function startEndTime(currentTime){
     let day = Math.floor(new Date().getTime() / 1e3) - (new Date(currentTime).getTime() / 1e3),
